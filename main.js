761b42c35d7cda21138977b942d2626cf88f7a81
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-10-30 16:31:30 -0500

    JavaScript conditional

--- main.js
+++ main.js
@@ -1,29 +1,13 @@
 document.querySelector('#check').addEventListener('click', check)
 
-    function check() {
+    function check () {
         const day = document.querySelector('#day').value.toLowerCase()
 
-        if(day === 'tuesday' || day === 'thursday') {
-            alert('You have class today')
-        }else if(day === 'saturday'|| day === 'sunday') {
-            alert('It\'s the weekend!')
-        }else if(day === 'monday' || day === 'wednesday') {
-            alert('These days are soooo long...')
-        }else{
-            alert('Boring!')
+        if (day === 'tuesday' || day === 'thursday') {
+            alert ('Class..........')
+        }else if (day === 'saturday' || day === 'sunday') {
+            alert ('Weekender fender bender')
+        }else {
+            alert ('Chale!')
         }
-}
-
-// document.querySelector('#check').addEventListener('click', check)
-
-//     function check() {
-//         const day = document.querySelector('#day').value.toLowerCase()
-
-//         if(day === 'tuesday' || day === 'thursday') {
-//             alert ('Class day')
-//         }else if (day === 'saturday' || day === 'sunday') {
-//             alert ('The Weekend!')
-//         }else {
-//             alert ('Nothin going on')
-//         }
-//     }
+    }

54ac23622a7b09e4a21da07458996f6e70398f68
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-10-31 17:05:54 -0500

    changed to alert

--- main.js
+++ main.js
@@ -1,32 +1,16 @@
-// document.querySelector('#check').addEventListener('click', check)
-
-//     function check () {
-//         const day = document.querySelector('#day').value.toLowerCase()
-
-//         if (day === 'tuesday' || day === 'thursday') {
-//             console.log('There is a class today')
-//         }else if (day === 'saturday' || day === 'sunday') {
-//             console.log('It is The Weekend!')
-//         }else if (day === 'monday') {
-//             console.log('What a looooong day!!!!')
-//         }else {
-//             console.log('Boring!!!')
-//         }
-//     }
-
 document.querySelector('#check').addEventListener('click', check)
 
     function check () {
         const day = document.querySelector('#day').value.toLowerCase()
 
         if (day === 'tuesday' || day === 'thursday') {
-            console.log('Today there will be a class!')
+            alert ('There is class today')
         }else if (day === 'saturday' || day === 'sunday') {
-            console.log('It\'s the weeekend, bruh!!')
-        }else if ( day === 'monday') {
-            console.log('The longest day of teh week')
+            alert ('The weekend is here!')
+        }else if (day === 'monday') {
+            alert ('Moncday\'s are sooooo looong')
         }else {
-            console.log('What in the world is going on in here??!')
+            alert ('Booorrrring!!')
         }
     }
 

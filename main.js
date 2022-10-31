5ead52d7121569c73afe569a5d491903fec04a1c
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-10-31 16:45:02 -0500

    More practice

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
+// document.querySelector('#check').addEventListener('click', check)
+
+//     function check () {
+//         const day = document.querySelector('#day').value.toLowerCase()
+
+//         if (day === 'tuesday' || day === 'thursday') {
+//             console.log('There is a class today')
+//         }else if (day === 'saturday' || day === 'sunday') {
+//             console.log('It is The Weekend!')
+//         }else if (day === 'monday') {
+//             console.log('What a looooong day!!!!')
+//         }else {
+//             console.log('Boring!!!')
+//         }
+//     }
+
 document.querySelector('#check').addEventListener('click', check)
 
     function check () {
         const day = document.querySelector('#day').value.toLowerCase()
 
         if (day === 'tuesday' || day === 'thursday') {
-            alert ('Class..........')
+            console.log('Today there will be a class!')
         }else if (day === 'saturday' || day === 'sunday') {
-            alert ('Weekender fender bender')
+            console.log('It\'s the weeekend, bruh!!')
+        }else if ( day === 'monday') {
+            console.log('The longest day of teh week')
         }else {
-            alert ('Chale!')
+            console.log('What in the world is going on in here??!')
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
